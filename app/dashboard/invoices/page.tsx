9707bd85e512b7a698b82f88576751078c2dcb81
TreePool113 <phuclinh777@gmail.com>
2025-09-08 18:27:27 +0700

    updated

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import NavLinks from "@/app/dashboard/nav-links";
+
+export default function page(){
+    return <p>Invoices Page</p>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import NavLinks from "@/app/dashboard/nav-links";
+
+export default function Page(){
+    return <p>Dashboard Page</p>;
+}
